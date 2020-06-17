57cd55a3ea2ac7ce7ed67673bade3935458d6dc4
Imke <imke.lansky@gmail.com>
2020-06-17 13:04:50 +0200

    Delete reproject_coords.sh

--- shell_scripts/reproject_coords.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-shopt -s nullglob
-for filename in *.geojson;
-do
-    echo "Processing $filename"
-    ogr2ogr -f "GeoJSON" ./Reprojected/ogr_$filename $filename -s_srs EPSG:4326 -t_srs EPSG:102004 -progress
-done
-shopt -u nullglob # Revert nullglob back to it's normal default state
